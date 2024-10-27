85748e7fc86a93dbd081e879103bd973aa3f531d
Joel Foster <janthonyf70@gmail.com>
2024-10-27 18:35:23 -0500

    Pushed Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let player1 = "X";
-    const squares = document.querySelectorAll("#board > div");
-    squares.forEach((square) => square.classList.add("square"));
+  let player1 = "X";
+  let player2 = "O";
+  let turn=1;
+  let gamearr=[];
+  const squares = document.querySelectorAll("#board > div");
+  squares.forEach((square) => square.classList.add("square"));
+
+  squares.forEach((square) => {
+    square.addEventListener("click", function(){
+      if (turn%2==1){
+        square.classList.add(player1);
+        square.innerHTML=player1;
+      } else {
+        square.classList.add(player2);
+        square.innerHTML=player2;
+      }
+      turn+=1;
+    });
   });
+
+
+
+
+
+
+
+
+
+
+
+
+});
