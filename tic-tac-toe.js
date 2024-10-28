49c57c86bf1161aec36b6259e2a577f7d0c57801
Joel Foster <janthonyf70@gmail.com>
2024-10-27 19:30:03 -0500

    Pushed Exercise 4

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,20 +2,27 @@ document.addEventListener("DOMContentLoaded", function () {
   let player1 = "X";
   let player2 = "O";
   let turn=1;
-  let gamearr=[];
+  let gamearr=[ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ];
+
   const squares = document.querySelectorAll("#board > div");
   squares.forEach((square) => square.classList.add("square"));
 
   squares.forEach((square) => {
     square.addEventListener("click", function(){
-      if (turn%2==1){
-        square.classList.add(player1);
-        square.innerHTML=player1;
-      } else {
-        square.classList.add(player2);
-        square.innerHTML=player2;
+      if (square.innerHTML==""){
+        if (turn%2==1){
+          square.classList.add(player1);
+          square.innerHTML=player1;
+        } else {
+          square.classList.add(player2);
+          square.innerHTML=player2;
+        }
+        turn+=1;
+        CheckforWin();
       }
-      turn+=1;
+      else{
+        window.alert("This square has already been marked!");
+      }
     });
   });
 
@@ -28,6 +35,22 @@ document.addEventListener("DOMContentLoaded", function () {
       square.classList.remove("hover");
     });
   });
+  
+  function CheckforWin(){
+    for (const combo of gamearr){
+      const [a,b,c] = combo;
+      if ((squares[a].classList.contains(player1))&&(squares[b].classList.contains(player1))&&
+      (squares[c].classList.contains(player1))){
+        document.getElementById("status").innerHTML = "Congratulations! X is the Winner!";
+        document.getElementById("status").classList.add("you-won");
+      }
+      else if ((squares[a].classList.contains(player2))&&(squares[b].classList.contains(player2))&&
+      (squares[c].classList.contains(player2))){
+        document.getElementById("status").innerHTML = "Congratulations! O is the Winner!";
+        document.getElementById("status").classList.add("you-won");
+      }
+    }
+  }
 
 
 
