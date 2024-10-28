645208ba7468dee2da72791e98605e31fbb6a15f
Joel Foster <janthonyf70@gmail.com>
2024-10-27 19:54:50 -0500

    Pushed Exercise 6

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   let player1 = "X";
   let player2 = "O";
-  let turn=1;
-  let gamearr=[ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ];
+  let turn=1;  //determines whether it is player 1 or 2's turn
+  let gamearr=[ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]; //winning combo array
   const butn = document.querySelector(".btn");
-  const stat = document.getElementById("status");
+  const stat = document.querySelector("#status");
 
   const squares = document.querySelectorAll("#board > div");
-  squares.forEach((square) => square.classList.add("square"));
+  squares.forEach((square) => square.classList.add("square"));  //Exercise 1
 
-  squares.forEach((square) => {
+  squares.forEach((square) => {                                 //Exercise 2
     square.addEventListener("click", function(){
       if (square.innerHTML==""){
         if (turn%2==1){
@@ -23,12 +23,12 @@ document.addEventListener("DOMContentLoaded", function () {
         CheckforWin();
       }
       else{
-        window.alert("This square has already been marked!");
+        window.alert("This square has already been marked!");     //Exercise 6
       }
     });
   });
 
-  squares.forEach((square) => {
+  squares.forEach((square) => {                                    //Exercise 3
     square.addEventListener("mouseover", function(){
       square.classList.add("hover");
     });
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
   
-  function CheckforWin(){
+  function CheckforWin(){                                         //Exercise 4
     for (const combo of gamearr){
       const [a,b,c] = combo;
       if ((squares[a].classList.contains(player1))&&(squares[b].classList.contains(player1))&&
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  butn.addEventListener("click", function(){
+  butn.addEventListener("click", function(){                       //Exercise 5
     squares.forEach((square) => {
       square.innerHTML="";
       square.classList.remove("X", "O");
